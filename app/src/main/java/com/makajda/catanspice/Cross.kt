dbcb5fad9e39b12361f150b29d714c9e9f941e62
Makajda <mcjdaaa@gmail.com>
2020-04-05 11:23:01 +0300

    add crosses

---
+++ app/src/main/java/com/makajda/catanspice/Cross.kt
@@ -0,0 +1,4 @@
+package com.makajda.catanspice
+
+class Cross(val slot1: Slot?, val slot2: Slot?, val slot3: Slot?) {
+}
--- app/src/main/java/com/makajda/catanspice/Map.kt
+++ app/src/main/java/com/makajda/catanspice/Map.kt
@@ -2,11 +2,17 @@ package com.makajda.catanspice
 
 class Map {
     val slots = ArrayList<Slot>()
+    val crosses = ArrayList<Cross>()
     val settlements = Array<Settlement>(Given.settlements.size * 2) {
             i -> Settlement(i / 2)
     }
 
     init {
+        createSlots();
+        createCrosses();
+    }
+
+    private fun createSlots() {
         for (q in 0..Given.edge) {
             for (r in 0..Given.edge) {
                 slots.add(Slot(q, -r))
@@ -21,5 +27,73 @@ class Map {
                 slots.add(Slot(-q, -r))
             }
         }
+    }
+
+    private fun createCrosses() {
+        val pairs = createPairs()
+        while (pairs.size > 0) {
+            val pair = pairs[0];
+            pairs.remove(pair);
+            addCross(pairs, pair.slot1, pair.slot2);
+            addCross(pairs, pair.slot2, pair.slot1);
+        }
+    }
+
+    private fun createPairs() : ArrayList<Pair> {
+        val pairs = ArrayList<Pair>()
+        for(slot in slots) {
+            val neighbors = getNeighbors(slot);
+            for (neighbor in neighbors) {
+                var exist = false;
+                for (pair in pairs) {
+                    if (pair.checkSame(slot, neighbor)) {
+                        exist = true;
+                        break;
+                    }
+                }
+                if(!exist) {
+                    pairs.add(Pair(slot, neighbor))
+                }
+            }
+        }
+        return pairs;
+    }
+
+    private fun addCross(pairs: ArrayList<Pair>, slot1: Slot?, slot2: Slot?) {
+        val neighbors1 = ArrayList<Pair>()
+        for(pair1 in pairs) {
+            if(pair1.slot1 == slot2) {
+                neighbors1.add(pair1)
+            }
+        }
+
+        for (pair1 in neighbors1) {
+            val neighbors2 = ArrayList<Pair>()
+            for(pair2 in pairs) {
+                if(
+                    pair2.slot1 == pair1.slot2 && pair2.slot2 == slot1 ||
+                    pair2.slot2 == pair1.slot2 && pair2.slot1 == slot1) {
+                    neighbors2.add(pair2)
+                }
+            }
+
+            if (neighbors2.size > 0) {
+                val p3 = neighbors2[0]
+                val slot3 = if(p3.slot1 == slot1) p3.slot2 else p3.slot1
+                crosses.add(Cross(slot1, slot2, slot3));
+            }
+        }
+    }
+
+    private fun getNeighbors(slot: Slot): ArrayList<Slot> {
+        val neighbors = ArrayList<Slot>()
+        for(n in slots) {
+            if(
+                slot.x == n.x && Math.abs(slot.y - n.y) == 1 ||
+                slot.y == n.y && Math.abs(slot.x - n.x) == 1 ||
+                slot.z == n.z && Math.abs(slot.x - n.x) == 1)
+                neighbors.add(n);
+        }
+        return neighbors;
     }
 }
