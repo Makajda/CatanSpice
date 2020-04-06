7073bc2d4b25cc68d36318d2f9f189633da0d82a
Makajda <mcjdaaa@gmail.com>
2020-04-06 14:52:35 +0300

    publish v.2

--- app/src/main/java/com/makajda/catanspice/Given.kt
+++ app/src/main/java/com/makajda/catanspice/Given.kt
@@ -8,8 +8,7 @@ object Given {
 
     //prod == 0 - it is a desert
     val prodsCount = intArrayOf(1, 4, 4, 4, 3, 3)
-    val prodsColor = arrayOf(0xFFF5DEB3, 0xFF008000, 0xFF90EE90, 0xFFFFFF00, 0xFFA86A13, 0xFFADD8E6)
-    //val prodsColor = arrayOf(0xFFF5DEB3, 0xFF006400, 0xFF90EE90, 0xFFFFFF00, 0xFF8B4513, 0xFFADD8E6)
+    val prodsColor = arrayOf(0xFFF5DEB3, 0xFF008000, 0xFFA0EDA0, 0xFFFFFF00, 0xFFA86A13, 0xFF6FCAE7)
     val jettonsCount = intArrayOf(1, 2, 2, 2, 2, 2, 2, 2, 2, 1)
     val jettonsValue = intArrayOf(2, 3, 4, 5, 6, 8, 9, 10, 11, 12)
     val settlements = intArrayOf(Color.RED, Color.BLUE, -23269, Color.WHITE) //-23269 = 0xFFA500
